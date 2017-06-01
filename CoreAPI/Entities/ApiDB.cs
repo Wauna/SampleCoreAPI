582dbdc40d84ed4e4492dcb989e3cc346f0243ff
Chad Theis <chadtheis@gmail.com>
2017-05-31 21:45:02 -0500

    Init

---
+++ CoreAPI/Entities/ApiDB.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+namespace CoreAPI.Entities
+{
+    public class ApiDB :DbContext
+    {
+
+        public ApiDB(DbContextOptions<ApiDB> options) : base(options) {
+            Database.Migrate();
+        }
+
+
+        //ADD DbSets HERE
+        public DbSet<object1> object1 { get; set; }
+
+
+
+    }
+}
---
+++ CoreAPI/Entities/object1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+
+//SAMPLE OBJECT
+namespace CoreAPI.Entities
+{
+    public class object1
+    {
+    }
+}
